1a2a639eee9277c92e7a70ed241ec49477a6ad63
Hossain Shaikh Saadi <shaikh.saadi@tum.de>
2022-03-14 16:00:16 +0100

    Update tokenize_and_align.sh

--- tokenize_and_align.sh
+++ tokenize_and_align.sh
@@ -53,9 +53,65 @@ fast_align/build/fast_align -i en-si/si-en.tokens.clean -d -o -v > en-si/si-en.a
 fast_align/build/fast_align -i en-si/si-en.tokens.clean -d -o -v -r > en-si/si-en.reverse.align
 fast_align/build/atools -i en-si/si-en.align -j en-si/si-en.reverse.align -c intersect > en-si/si-en.alignment
 
-python train_test_val.py --tokens_file en-sw/sw-en.tokens.clean --alignment_file en-sw/sw-en.alignment --language sw
-python train_test_val.py --tokens_file en-sd/sd-en.tokens.clean --alignment_file en-sd/sd-en.alignment --language sd
-python train_test_val.py --tokens_file en-si/si-en.tokens.clean --alignment_file en-si/si-en.alignment --language si
-python train_test_val.py --tokens_file en-mi/mi-en.tokens.clean --alignment_file en-mi/mi-en.alignment --language mi
-python train_test_val.py --tokens_file en-ml/ml-en.tokens.clean --alignment_file en-ml/ml-en.alignment --language ml
-python train_test_val.py --tokens_file en-am/am-en.tokens.clean --alignment_file en-am/am-en.alignment --language am
+
+mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < en-af/en-af.en > en-af/en-af.en.token
+mosesdecoder/scripts/tokenizer/tokenizer.perl -q --no-escape -l af < en-af/en-af.af > en-af/en-af.af.token
+python fastalign_data.py --src_file en-af/en-af.af.token --tgt_file en-af/en-af.en.token --output_file en-af/af-en.tokens
+sed 's/( .. ) //g' en-af/af-en.tokens | grep -F -v "\\" | grep -P -v "[\xAD]" | sed "s/&apos;/'/g" | sed 's/&quot;/"/g' | sed 's/&#91;/[/g' | sed 's/&#93;/]/g' | sed 's/&amp;/\&/g' | grep -v "=" | grep -v "#" | grep -v "http" | grep -v "&gt;" | grep -v "&lt;" | sed '/^ |||/ d' | sed '/||| $/d' | sed '/^|||/ d' | sed '/|||$/d' > en-af/af-en.tokens.clean
+
+fast_align/build/fast_align -i en-af/af-en.tokens.clean -d -o -v > en-af/af-en.align
+fast_align/build/fast_align -i en-af/af-en.tokens.clean -d -o -v -r > en-af/af-en.reverse.align
+fast_align/build/atools -i en-af/af-en.align -j en-af/af-en.reverse.align -c intersect > en-af/af-en.alignment
+
+
+mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < en-mk/en-mk.en > en-mk/en-mk.en.token
+mosesdecoder/scripts/tokenizer/tokenizer.perl -q --no-escape -l mk < en-mk/en-mk.mk > en-mk/en-mk.mk.token
+python fastalign_data.py --src_file en-mk/en-mk.mk.token --tgt_file en-mk/en-mk.en.token --output_file en-mk/mk-en.tokens
+sed 's/( .. ) //g' en-mk/mk-en.tokens | grep -F -v "\\" | grep -P -v "[\xAD]" | sed "s/&apos;/'/g" | sed 's/&quot;/"/g' | sed 's/&#91;/[/g' | sed 's/&#93;/]/g' | sed 's/&amp;/\&/g' | grep -v "=" | grep -v "#" | grep -v "http" | grep -v "&gt;" | grep -v "&lt;" | sed '/^ |||/ d' | sed '/||| $/d' | sed '/^|||/ d' | sed '/|||$/d' > en-mk/mk-en.tokens.clean
+
+fast_align/build/fast_align -i en-mk/mk-en.tokens.clean -d -o -v > en-mk/mk-en.align
+fast_align/build/fast_align -i en-mk/mk-en.tokens.clean -d -o -v -r > en-mk/mk-en.reverse.align
+fast_align/build/atools -i en-mk/mk-en.align -j en-mk/mk-en.reverse.align -c intersect > en-mk/mk-en.alignment
+
+mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < en-eu/en-eu.en > en-eu/en-eu.en.token
+mosesdecoder/scripts/tokenizer/tokenizer.perl -q --no-escape -l eu < en-eu/en-eu.eu > en-eu/en-eu.eu.token
+python fastalign_data.py --src_file en-eu/en-eu.eu.token --tgt_file en-eu/en-eu.en.token --output_file en-eu/eu-en.tokens
+sed 's/( .. ) //g' en-eu/eu-en.tokens | grep -F -v "\\" | grep -P -v "[\xAD]" | sed "s/&apos;/'/g" | sed 's/&quot;/"/g' | sed 's/&#91;/[/g' | sed 's/&#93;/]/g' | sed 's/&amp;/\&/g' | grep -v "=" | grep -v "#" | grep -v "http" | grep -v "&gt;" | grep -v "&lt;" | sed '/^ |||/ d' | sed '/||| $/d' | sed '/^|||/ d' | sed '/|||$/d' > en-eu/eu-en.tokens.clean
+
+fast_align/build/fast_align -i en-eu/eu-en.tokens.clean -d -o -v > en-eu/eu-en.align
+fast_align/build/fast_align -i en-eu/eu-en.tokens.clean -d -o -v -r > en-eu/eu-en.reverse.align
+fast_align/build/atools -i en-eu/eu-en.align -j en-eu/eu-en.reverse.align -c intersect > en-eu/eu-en.alignment
+
+mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < en-bg/en-bg.en > en-bg/en-bg.en.token
+mosesdecoder/scripts/tokenizer/tokenizer.perl -q --no-escape -l bg < en-bg/en-bg.bg > en-bg/en-bg.bg.token
+python fastalign_data.py --src_file en-bg/en-bg.bg.token --tgt_file en-bg/en-bg.en.token --output_file en-bg/bg-en.tokens
+sed 's/( .. ) //g' en-bg/bg-en.tokens | grep -F -v "\\" | grep -P -v "[\xAD]" | sed "s/&apos;/'/g" | sed 's/&quot;/"/g' | sed 's/&#91;/[/g' | sed 's/&#93;/]/g' | sed 's/&amp;/\&/g' | grep -v "=" | grep -v "#" | grep -v "http" | grep -v "&gt;" | grep -v "&lt;" | sed '/^ |||/ d' | sed '/||| $/d' | sed '/^|||/ d' | sed '/|||$/d' > en-bg/bg-en.tokens.clean
+
+fast_align/build/fast_align -i en-bg/bg-en.tokens.clean -d -o -v > en-bg/bg-en.align
+fast_align/build/fast_align -i en-bg/bg-en.tokens.clean -d -o -v -r > en-bg/bg-en.reverse.align
+fast_align/build/atools -i en-bg/bg-en.align -j en-bg/bg-en.reverse.align -c intersect > en-bg/bg-en.alignment
+
+mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < en-gu/en-gu.en > en-gu/en-gu.en.token
+python tokenize_data.py --input_file en-gu/en-gu.gu --output_file en-gu/en-gu.gu.token --language gu
+python fastalign_data.py --src_file en-gu/en-gu.gu.token --tgt_file en-gu/en-gu.en.token --output_file en-gu/gu-en.tokens
+sed 's/( .. ) //g' en-gu/gu-en.tokens | grep -F -v "\\" | grep -P -v "[\xAD]" | sed "s/&apos;/'/g" | sed 's/&quot;/"/g' | sed 's/&#91;/[/g' | sed 's/&#93;/]/g' | sed 's/&amp;/\&/g' | grep -v "=" | grep -v "#" | grep -v "http" | grep -v "&gt;" | grep -v "&lt;" | sed '/^ |||/ d' | sed '/||| $/d' | sed '/^|||/ d' | sed '/|||$/d' > en-gu/gu-en.tokens.clean
+
+fast_align/build/fast_align -i en-gu/gu-en.tokens.clean -d -o -v > en-gu/gu-en.align
+fast_align/build/fast_align -i en-gu/gu-en.tokens.clean -d -o -v -r > en-gu/gu-en.reverse.align
+fast_align/build/atools -i en-gu/gu-en.align -j en-gu/gu-en.reverse.align -c intersect > en-gu/gu-en.alignment
+
+mosesdecoder/scripts/tokenizer/tokenizer.perl -l en < en-kn/en-kn.en > en-kn/en-kn.en.token
+python tokenize_data.py --input_file en-kn/en-kn.kn --output_file en-kn/en-kn.kn.token --language kn
+python fastalign_data.py --src_file en-kn/en-kn.kn.token --tgt_file en-kn/en-kn.en.token --output_file en-kn/kn-en.tokens
+sed 's/( .. ) //g' en-kn/kn-en.tokens | grep -F -v "\\" | grep -P -v "[\xAD]" | sed "s/&apos;/'/g" | sed 's/&quot;/"/g' | sed 's/&#91;/[/g' | sed 's/&#93;/]/g' | sed 's/&amp;/\&/g' | grep -v "=" | grep -v "#" | grep -v "http" | grep -v "&gt;" | grep -v "&lt;" | sed '/^ |||/ d' | sed '/||| $/d' | sed '/^|||/ d' | sed '/|||$/d' > en-kn/kn-en.tokens.clean
+
+fast_align/build/fast_align -i en-kn/kn-en.tokens.clean -d -o -v > en-kn/kn-en.align
+fast_align/build/fast_align -i en-kn/kn-en.tokens.clean -d -o -v -r > en-kn/kn-en.reverse.align
+fast_align/build/atools -i en-kn/kn-en.align -j en-kn/kn-en.reverse.align -c intersect > en-kn/kn-en.alignment
+
+#python train_test_val.py --tokens_file en-sw/sw-en.tokens.clean --alignment_file en-sw/sw-en.alignment --language sw
+#python train_test_val.py --tokens_file en-sd/sd-en.tokens.clean --alignment_file en-sd/sd-en.alignment --language sd
+#python train_test_val.py --tokens_file en-si/si-en.tokens.clean --alignment_file en-si/si-en.alignment --language si
+#python train_test_val.py --tokens_file en-mi/mi-en.tokens.clean --alignment_file en-mi/mi-en.alignment --language mi
+#python train_test_val.py --tokens_file en-ml/ml-en.tokens.clean --alignment_file en-ml/ml-en.alignment --language ml
+#python train_test_val.py --tokens_file en-am/am-en.tokens.clean --alignment_file en-am/am-en.alignment --language am
